5e84938241e6ab5bd1d39404ed289c1636de0aee
mohamed elbohy <malbohy@gmail.com>
2017-11-30 13:33:14 +0200

    finish project components
    - add loading view
    - edit favourite view
    - add search view
    - add user reposetory
    - update app delegt root screen

---
+++ MVC-IOS/Source/UI/Search/SearchViewController.swift
@@ -0,0 +1,209 @@
+//
+//  SearchViewController.swift
+//  MVC-IOS
+//
+//  Created by mohamed albohy on 11/30/17.
+//  Copyright © 2017 Mohamed Elbohy. All rights reserved.
+//
+
+import UIKit
+import GithubKit
+import NoticeObserveKit
+
+final class SearchViewController: UIViewController {
+    
+    @IBOutlet weak var totalCountLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var tableViewBottomConstraint: NSLayoutConstraint!
+    
+    
+    
+    let searchBar = UISearchBar(frame: .zero)
+    let loadingView = LoadingView.makeFromNib()
+    
+    private var pool = Notice.ObserverPool()
+    private var isReachedBottom: Bool = false {
+        didSet {
+            if isReachedBottom && isReachedBottom != oldValue {
+                searchModel.fetchUsers()
+            }
+        }
+    }
+    
+    let favoriteModel: FavoriteModel
+    let searchModel: SearchModel
+    
+    init(searchModel: SearchModel, favoriteModel: FavoriteModel) {
+        self.searchModel = searchModel
+        self.favoriteModel = favoriteModel
+        super.init(nibName: SearchViewController.className, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.titleView = searchBar
+        searchBar.delegate = self
+        searchBar.placeholder = "Input user name"
+        
+        configure(with: tableView)
+        
+        searchModel.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        observeKeyboard()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if searchBar.isFirstResponder {
+            searchBar.resignFirstResponder()
+        }
+        pool = Notice.ObserverPool()
+    }
+    
+    private func configure(with tableView: UITableView) {
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.register(UserViewCell.self)
+        tableView.register(UITableViewHeaderFooterView.self, forHeaderFooterViewReuseIdentifier: UITableViewHeaderFooterView.className)
+    }
+    
+    private func observeKeyboard() {
+        NotificationCenter.default.nok.observe(name: .keyboardWillShow) { [weak self] in
+            self?.view.layoutIfNeeded()
+            let extra = self?.tabBarController?.tabBar.bounds.height ?? 0
+            self?.tableViewBottomConstraint.constant = $0.frame.size.height - extra
+            UIView.animate(withDuration: $0.animationDuration, delay: 0, options: $0.animationCurve, animations: {
+                self?.view.layoutIfNeeded()
+            }, completion: nil)
+            }
+            .invalidated(by: pool)
+        
+        NotificationCenter.default.nok.observe(name: .keyboardWillHide) { [weak self] in
+            self?.view.layoutIfNeeded()
+            self?.tableViewBottomConstraint.constant = 0
+            UIView.animate(withDuration: $0.animationDuration, delay: 0, options: $0.animationCurve, animations: {
+                self?.view.layoutIfNeeded()
+            }, completion: nil)
+            }
+            .invalidated(by: pool)
+    }
+    
+    private func showUserRepository(with user: User) {
+        let repositoryModel = RepositoryModel(user: user)
+        let vc = UserRepositoryViewController(repositoryModel: repositoryModel, favoriteModel: favoriteModel)
+        navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
+extension SearchViewController: SearchModelDelegate {
+    func searchModel(_ searchModel: SearchModel, didRecieve errorMessage: ErrorMessage) {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: errorMessage.title,
+                                          message: errorMessage.message,
+                                          preferredStyle: .alert)
+            self.present(alert, animated: false, completion: nil)
+        }
+    }
+    
+    func searchModel(_ searchModel: SearchModel, didChange isFetchingUsers: Bool) {
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+    
+    func searchModel(_ searchModel: SearchModel, didChange users: [User]) {
+        let totalCount = searchModel.totalCount
+        DispatchQueue.main.async {
+            self.totalCountLabel.text = "\(users.count) / \(totalCount)"
+            self.tableView.reloadData()
+        }
+    }
+    
+    func searchModel(_ searchModel: SearchModel, didChange totalCount: Int) {
+        let users = searchModel.users
+        DispatchQueue.main.async {
+            self.totalCountLabel.text = "\(users.count) / \(totalCount)"
+        }
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        searchBar.showsCancelButton = false
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        searchBar.showsCancelButton = false
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = true
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        searchModel.fetchUsers(withQuery: searchText)
+    }
+}
+
+extension SearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchModel.users.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeue(UserViewCell.self, for: indexPath)
+        cell.configure(with: searchModel.users[indexPath.row])
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return nil
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        guard let view = tableView.dequeueReusableHeaderFooterView(withIdentifier: UITableViewHeaderFooterView.className) else {
+            return nil
+        }
+        loadingView.removeFromSuperview()
+        loadingView.isLoading = searchModel.isFetchingUsers
+        loadingView.add(to: view)
+        return view
+    }
+}
+
+extension SearchViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: false)
+        
+        let user = searchModel.users[indexPath.row]
+        showUserRepository(with: user)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UserViewCell.calculateHeight(with: searchModel.users[indexPath.row], and: tableView)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return .leastNormalMagnitude
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return searchModel.isFetchingUsers ? LoadingView.defaultHeight : .leastNormalMagnitude
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let maxScrollDistance = max(0, scrollView.contentSize.height - scrollView.bounds.size.height)
+        isReachedBottom = maxScrollDistance <= scrollView.contentOffset.y
+    }
+}
---
+++ MVC-IOS/Source/UI/UserRepository/UserRepositoryViewController.swift
@@ -0,0 +1,143 @@
+//
+//  UserRepositoryViewController.swift
+//  MVC-IOS
+//
+//  Created by mohamed albohy on 11/30/17.
+//  Copyright © 2017 Mohamed Elbohy. All rights reserved.
+//
+
+import Foundation
+
+
+import UIKit
+import GithubKit
+
+final class UserRepositoryViewController: UIViewController {
+    
+    @IBOutlet private(set) weak var tableView: UITableView!
+    @IBOutlet private(set) weak var totalCountLabel: UILabel!
+    
+    let loadingView = LoadingView.makeFromNib()
+    
+    private var isReachedBottom: Bool = false {
+        didSet {
+            if isReachedBottom && isReachedBottom != oldValue {
+                repositoryModel.fetchRepositories()
+            }
+        }
+    }
+    
+    let favoriteModel: FavoriteModel
+    let repositoryModel: RepositoryModel
+    
+    init(repositoryModel: RepositoryModel, favoriteModel: FavoriteModel) {
+        self.repositoryModel = repositoryModel
+        self.favoriteModel = favoriteModel
+        
+        super.init(nibName: UserRepositoryViewController.className, bundle: nil)
+        hidesBottomBarWhenPushed = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "\(repositoryModel.user.login)'s Repositories"
+        
+        configure(with: tableView)
+        
+        repositoryModel.delegate = self
+        repositoryModel.fetchRepositories()
+    }
+    
+    private func configure(with tableView: UITableView) {
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.register(RepositoryViewCell.self)
+        tableView.register(UITableViewHeaderFooterView.self, forHeaderFooterViewReuseIdentifier: UITableViewHeaderFooterView.className)
+    }
+    
+    private func showRepository(with repository: Repository) {
+        let vc = RepositoryViewController(repository: repository, favoriteModel: favoriteModel)
+        navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
+extension UserRepositoryViewController: RepositoryModelDelegate {
+    func repositoryModel(_ repositoryModel: RepositoryModel, didChange isFetchingRepositories: Bool) {
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+    
+    func repositoryModel(_ repositoryModel: RepositoryModel, didChange repositories: [Repository]) {
+        let totalCount = repositoryModel.totalCount
+        DispatchQueue.main.async {
+            self.totalCountLabel.text = "\(repositories.count) / \(totalCount)"
+            self.tableView.reloadData()
+        }
+    }
+    
+    func repositoryModel(_ repositoryModel: RepositoryModel, didChange totalCount: Int) {
+        let repositories = repositoryModel.repositories
+        DispatchQueue.main.async {
+            self.totalCountLabel.text = "\(repositories.count) / \(totalCount)"
+        }
+    }
+}
+
+extension UserRepositoryViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return repositoryModel.repositories.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeue(RepositoryViewCell.self, for: indexPath)
+        cell.configure(with: repositoryModel.repositories[indexPath.row])
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return nil
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        guard let view = tableView.dequeueReusableHeaderFooterView(withIdentifier: UITableViewHeaderFooterView.className) else {
+            return nil
+        }
+        loadingView.removeFromSuperview()
+        loadingView.isLoading = repositoryModel.isFetchingRepositories
+        loadingView.add(to: view)
+        return view
+    }
+}
+
+extension UserRepositoryViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: false)
+        
+        let repository = repositoryModel.repositories[indexPath.row]
+        showRepository(with: repository)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return RepositoryViewCell.calculateHeight(with: repositoryModel.repositories[indexPath.row], and: tableView)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return .leastNormalMagnitude
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return repositoryModel.isFetchingRepositories ? LoadingView.defaultHeight : .leastNormalMagnitude
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let maxScrollDistance = max(0, scrollView.contentSize.height - scrollView.bounds.size.height)
+        isReachedBottom = maxScrollDistance <= scrollView.contentOffset.y
+    }
+}
