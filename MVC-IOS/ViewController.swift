9b27eec8e7209c39079e1f9f05a9269a0d13c10d
mohamed elbohy <mohamedelbohey@paymobsolutions.com>
2017-11-30 12:29:00 +0200

    create project

---
+++ MVC-IOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVC-IOS
+//
+//  Created by mohamed albohy on 11/30/17.
+//  Copyright © 2017 Mohamed Elbohy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
